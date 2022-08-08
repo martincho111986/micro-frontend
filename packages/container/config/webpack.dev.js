10b2b181a72c92cf51c6d3469b4e269f5facfa42
Martin Jerez <martin.jerez@globallogic.com>
2022-08-08 18:17:34 -0300

    created container deployment script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,10 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
-
-
 
 const devConfig = {
     mode: 'development',
@@ -22,9 +19,6 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
     ]
 };
 
